ad8a974442424ab33d2d9c3c05bd06572cef6b25
Bhupen <Bhupen>
2021-07-22 07:43:00 +0530

    commiting

---
+++ src/pages/register.page.ts
@@ -0,0 +1,58 @@
+import { click, selectDropdown, selectVisibleText, setText } from '../utils/commands';
+import Page from './page'
+
+class ChaiRegister extends Page {
+
+    private get header() { return $("//h1"); }
+    private get fname() { return $('[name=firstname]') }
+    private get lname() { return $('[name=lastname]') }
+    private get gender_radio() { return $$('[name=sex]') }
+    private get experience_radio() { return $$('[name=exp]') }
+    private get favchai_checkbox() { return $$('input[name*=Tea]') }
+    private get whychai_checkbox() { return $$('[name=tool]') }
+    private get continent_dropdown() { return $('#continents') }
+    private get selCommands_multiselect() { return $('#selenium_commands') }
+    private get submit_btn() { return $('#submit') }
+
+    getHeader() {
+        return this.header
+    }
+
+    async enterFirstName(firstname:string) {
+        await setText(this.fname, firstname)
+    }
+
+    async enterLastName(lastname:string) {
+        await setText(this.lname, lastname)
+    }
+
+    async selectGender(gender: string) {
+        await selectDropdown(this.gender_radio, gender)
+    }
+
+    async selectExperience(years: string) {
+        await selectDropdown(this.experience_radio, years)
+    }
+
+    async selectFavChai(chaiType: string) {
+        await selectDropdown(this.favchai_checkbox, chaiType)
+    }
+
+    async selectReason(reason:string) {
+        await selectDropdown(this.whychai_checkbox, reason)
+    }
+
+    async selectContinent(continent:string) {
+        await selectVisibleText(this.continent_dropdown, continent)
+    }
+
+    async selectSeleniumCommand(command:string) {
+        await selectVisibleText(this.selCommands_multiselect, command)
+    }
+
+    async clickOnSubmitBtn() {
+        await click(this.submit_btn)
+    }
+
+}
+export default new ChaiRegister()
---
+++ test/steps/register.steps.ts
@@ -0,0 +1,32 @@
+import { Given, When, Then } from '@cucumber/cucumber'
+import chaiPage from '../../src/pages/register.page'
+
+Given(/^I am on practice page \"([^\"]*)\"$/, async (appurl: string) => {
+    await browser.maximizeWindow()
+    await browser.url(appurl)
+});
+
+Then(/^I validat page header \"([^\"]*)\"$/, async (header: string) => {
+    await expect(chaiPage.getHeader()).toHaveText(header)
+});
+
+When(/^I enter firstname (.+) and lastname (.+)$/, async (fname: string, lname: string) => {
+    await chaiPage.enterFirstName(fname)
+    await chaiPage.enterLastName(lname)
+});
+
+When(/^I select gender (.+) years (.+) favorite chai (.+) and reason (.+)$/, async (gender: string, yrs: string, favchai: string, reason: string) => {
+    await chaiPage.selectGender(gender)
+    await chaiPage.selectExperience(yrs)
+    await chaiPage.selectFavChai(favchai)
+    await chaiPage.selectReason(reason)
+});
+
+When(/^I select continent (.+) and commands (.+)$/, async (continent: string, command: string) => {
+    await chaiPage.selectContinent(continent)
+    await chaiPage.selectSeleniumCommand(command)
+});
+
+When(/^I click on submit button$/, async () => {
+    await chaiPage.clickOnSubmitBtn()
+});
